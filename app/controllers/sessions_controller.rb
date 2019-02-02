5f032733ed6cdfb6ada6757c3f79fd5decf8e34e
EC2 Default User <ec2-user@ip-172-31-76-98.ec2.internal>
2019-02-02 06:54:31 +0000

    login

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,35 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    name = params[:session][:name]
+    password = params[:session][:password]
+    if login(name, password)
+      flash[:success] = 'ログインに成功しました。'
+      redirect_to @admin
+    else
+      flash.now[:danger] = 'ログインに失敗しました。'
+      render 'new'
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:success] = 'ログアウトしました。'
+    redirect_to root_url
+  end
+  
+  private
+
+  def login(name, password)
+    @admin = Admin.find_by(name: name)
+    if @admin && @admin.authenticate(password)
+      session[:admin_id] = @admin.id
+      return true
+    else
+      return false
+    end
+  end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
  root to: 'registered_companies_pages#index'
+ 
+ get 'login', to: 'sessions#new'
+ post 'login', to: 'sessions#create'
+ delete 'logout', to: 'sessions#destroy'
+ 
 end
