6e4d0059099c96d741ec0ad2585e0718dac7dfab
EC2 Default User <ec2-user@ip-172-31-19-76.ec2.internal>
2019-02-10 01:44:23 +0000

    change form layout

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,18 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+(1..10).each do |number|
+  Company.create(name: '株式会社' + number.to_s, ceo: '田中1', established_yearmonth: "1992年1月1日" + number.to_s, telephone: "03-1234-5678")
+end
 
 (1..10).each do |number|
-  Member.create(name: '山田' + number.to_s, company_id: number.to_s)
+  Recruit.create(title: 'エンジニア募集! ' + number.to_s, company_id: number.to_s)
+end
+
+(1..10).each do |number|
+  Member.create(name: '山田' + number.to_s, sex: '男性', email: 'a@gmail.com' + number.to_s, password: 'aaaaaa'+ number.to_s, password_confirmation: 'aaaaaa'+ number.to_s, company_id: number.to_s)
+end
+
+(1..10).each do |number|
+  User.create(name: '田中' + number.to_s, sex: '男性', birthday: "1992年1月1日", email: 'b@gmail.com' + number.to_s, password: 'bbbbbb'+ number.to_s, password_confirmation: 'bbbbbb'+ number.to_s, company_id: number.to_s)
 end
